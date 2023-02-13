11086d7c31727971b910bb0756ef1c86a7d6e7ff
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-13 19:11:03 +0000

    feat(header): add content and styling to header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Logo from "../images/logo.png";
+
+const Header = () => {
+	return (
+		<header>
+      <img src={Logo} alt="Logo for kportfolio" />
+      <button>Say Hello 👋</button>
+		</header>
+	);
+};
+
+export default Header;
