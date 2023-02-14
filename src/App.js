8feda5cd06f994826d20cb3c56467a72f80ebb10
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-14 17:14:31 +0000

    feat(quote): add and style quote

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
     <div className="container">
       <Header />
       <Hero />
+      <div className='content'>
+        <div className='quote'>
+          <p>
+            <span>&quot;</span>My passion for web development runs deep, and I&apos;m constantly exploring the latest tools and technologies to stay ahead of the curve. From designing responsive layouts to writing clean, efficient code, I&apos;m always striving to deliver the best possible user experience.<span>&quot;</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
