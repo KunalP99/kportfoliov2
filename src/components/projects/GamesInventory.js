ae309ef3b3c20b018e81b1568241cac6bba1034d
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-14 19:49:11 +0000

    feat: style games inventory card and add content to card

--- src/components/projects/GamesInventory.js
+++ src/components/projects/GamesInventory.js
@@ -1,16 +1,22 @@
 import OrangeArrow from '../../images/projects/orange-arrow.svg';
 import WhiteArrow from '../../images/projects/white-arrow.svg';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow } from 'swiper';
+import { Autoplay } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
 
 // Images
 import InventoryImg1 from '../../images/projects/inventory/inventory-img-1.png';
 import InventoryImg2 from '../../images/projects/inventory/inventory-img-2.png';
 import InventoryImg3 from '../../images/projects/inventory/inventory-img-3.png';
+import InventoryImg4 from '../../images/projects/inventory/inventory-img-4.png';
+import InventoryImg5 from '../../images/projects/inventory/inventory-img-5.png';
+import InventoryImg6 from '../../images/projects/inventory/inventory-img-6.png';
+import InventoryImg7 from '../../images/projects/inventory/inventory-img-7.png';
+import InventoryImg8 from '../../images/projects/inventory/inventory-img-8.png';
+import InventoryImg9 from '../../images/projects/inventory/inventory-img-9.png';
+import InventoryImg10 from '../../images/projects/inventory/inventory-img-10.png';
 
 const GamesInventory = () => {
   return (
@@ -22,16 +28,24 @@ const GamesInventory = () => {
         <a className="github-link" href="https://github.com/KunalP99/inventory-management" target='_blank' rel="noreferrer">GitHub  <img src={WhiteArrow} alt="Visit games inventory GitHub" /></a>
       </div>
       <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
+        loop={true}
         centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          stretch: 0,
-          modifier: 1,
+        slidesPerView={3}
+        speed={9000}
+        simulateTouch={false}
+        breakpoints={{
+          500: {
+            slidesPerView: 4
+          },
+          800: {
+            slidesPerView: 5
+          }
         }}
-        pagination={true}
-        modules={[EffectCoverflow]}
+        autoplay={{
+          delay: 1,
+          reverseDirection: false,
+        }}
+        modules={[Autoplay]}
         className="mySwiper"
       >
         <SwiperSlide>
@@ -42,6 +56,27 @@ const GamesInventory = () => {
         </SwiperSlide>
         <SwiperSlide>
           <img src={InventoryImg3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg4} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg5} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg6} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg7} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg8} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg9} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={InventoryImg10} />
         </SwiperSlide>
       </Swiper>
     </div>
