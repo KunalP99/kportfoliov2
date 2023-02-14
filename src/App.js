168297456779280e1f86c7a1f3a76d28974c9946
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-14 17:41:40 +0000

    feat: add ProjectsGrid component to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import ProjectsGrid from './components/ProjectsGrid.';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <span>&quot;</span>My passion for web development runs deep, and I&apos;m constantly exploring the latest tools and technologies to stay ahead of the curve. From designing responsive layouts to writing clean, efficient code, I&apos;m always striving to deliver the best possible user experience.<span>&quot;</span>
           </p>
         </div>
+        <ProjectsGrid />
       </div>
     </div>
   );
