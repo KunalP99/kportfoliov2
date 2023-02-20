18b13fe55e6517f00f2a5d797e762cab6310dad7
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-20 15:44:26 +0000

    feat: add pictures and videos for the about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,17 @@ import 'swiper/css';
 // Images
 import AboutImg1 from '../images/about/about-img-1.jpg';
 import AboutImg2 from '../images/about/about-img-2.jpg';
+import AboutImg3 from '../images/about/about-img-3.jpg';
+import AboutImg4 from '../images/about/about-img-4.jpg';
+import AboutImg5 from '../images/about/about-img-5.jpg';
+import AboutImg6 from '../images/about/about-img-6.jpg';
 
 // Video 
 import AboutVid1 from '../images/about/about-vid-1.mp4';
+import AboutVid2 from '../images/about/about-vid-2.mp4';
 
 
 const About = () => {
-
-  // const aboutSwiper = new Swiper('.s2', {});
-
-  // console.log(document.querySelector('.swiper'));
-
   return (
     <div className="about-container">
       <h2>About Me</h2>
@@ -45,19 +45,22 @@ const About = () => {
           <img src={AboutImg2} />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={AboutImg2} />
+          <video src={AboutVid1} autoPlay={true} loop={true} muted />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={AboutImg2} />
+          <img src={AboutImg3} />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={AboutImg2} />
+          <img src={AboutImg4} />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={AboutImg2} />
+          <img src={AboutImg5} />
         </SwiperSlide>
         <SwiperSlide>
-          <video src={AboutVid1} autoPlay="true" loop="true" muted />
+          <video src={AboutVid2} autoPlay={true} loop={true} muted />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={AboutImg6} />
         </SwiperSlide>
       </Swiper>
     </div>
