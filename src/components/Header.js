e18d94df0f062f808d7ea82078c39e33ee6f87ab
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-22 18:07:53 +0000

    feat: implement dark mode and add button that can switch between dark and light mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import HamburgerMenu from '../images/hamburger.svg';
 import xIcon from '../images/x.svg';
+import DarkMode from '../images/darklightmode/dark-mode.svg';
+import LightMode from '../images/darklightmode/light-mode.svg';
 
 const Header = ({scrollIntoViewOffset}) => {
   const [navIcon, setNavIcon] = useState(HamburgerMenu);
+  const [theme, setTheme] = useState(DarkMode);
 
   const toggleNav = () => {
     // Change hamburger menu to x icon and vice versa
@@ -15,6 +18,20 @@ const Header = ({scrollIntoViewOffset}) => {
 
     const navbarLinks = document.querySelector('.navbar-links');
     navbarLinks.classList.toggle('active');
+  };
+
+  // * TODO: Add dark mode icon and switch between moon and sun or similiar
+  const toggleDarkMode = () => {
+    const html = document.querySelector('html');
+    if (html.style.background === 'white'){
+      html.style.background = '#1A1A1A';
+      html.style.color = 'white';
+      setTheme(LightMode);
+    }  else {
+      html.style.background = 'white';
+      html.style.color = 'black';
+      setTheme(DarkMode);
+    }
   };
 
   return (
@@ -29,6 +46,7 @@ const Header = ({scrollIntoViewOffset}) => {
             <li><button type='button' onClick={() => scrollIntoViewOffset('#about', 100)}>About</button></li>
             <li><button type='button'>Contact</button></li>
             <li><a className='action-btn' href='mailto:kunal_patel200@hotmail.co.uk'>Say Hello 👋</a></li>
+            <li><button onClick={toggleDarkMode}><img src={theme} alt="Click to change colour of website" /></button></li>
           </ul>
         </div>
         <button className='hamburger' type='button' onClick={toggleNav}><img src={navIcon} alt="Open hamburger menu" /></button>
