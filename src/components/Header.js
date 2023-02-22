01d98d95b327102167fb1e00c3d457f564da8b52
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-02-22 15:08:24 +0000

    feat(header): overhaul navbar,change logo, add sidebar for when screensize is 700px or below

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,38 @@
-import Logo from '../images/logo.png';
+import { useState } from 'react';
+import HamburgerMenu from '../images/hamburger.svg';
+import xIcon from '../images/x.svg';
 
 const Header = () => {
+  const [navIcon, setNavIcon] = useState(HamburgerMenu);
+
+  const toggleNav = () => {
+    // Change hamburger menu to x icon and vice versa
+    if (navIcon !== HamburgerMenu) {
+      setNavIcon(HamburgerMenu);
+    } else {
+      setNavIcon(xIcon);
+    }
+
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.classList.toggle('active');
+  };
+
   return (
     <header id='header'>
-      <img src={Logo} alt="Logo for kportfolio" />
-      <a href='mailto:kunal_patel200@hotmail.co.uk'>Say Hello 👋</a>
+      <nav>
+        <div className="logo">
+          <h3>KP</h3>
+        </div>
+        <div className="navbar-links">
+          <ul>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a className='action-btn' href='mailto:kunal_patel200@hotmail.co.uk'>Say Hello 👋</a></li>
+          </ul>
+        </div>
+        <button className='hamburger' type='button' onClick={toggleNav}><img src={navIcon} alt="Open hamburger menu" /></button>
+      </nav>
     </header>
   );
 };
