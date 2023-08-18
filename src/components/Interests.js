a9a73393d642643b0af6b7588977950c4d06a143
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-08-18 12:38:01 +0100

    feat: add more interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,13 +4,23 @@ import LegoIcon from '../images/interests/lego.png';
 import SteamIcon from '../images/interests/steam.png';
 import FoodIcon from '../images/interests/food.png';
 import CocaColaIcon from '../images/interests/coca-cola.png';
-import VegasIcon from '../images/interests/vegas.png';
 import NeptuneIcon from '../images/interests/neptune.png';
 import WheelIcon from '../images/interests/wheel.png';
-import UniqloIcon from '../images/interests/uniqlo.png';
+import GuitarIcon from '../images/interests/guitar.png';
+import PopcornIcon from '../images/interests/popcorn.png';
 
 const Interests = () => {
-  const imgArr = [BbIcon, LegoIcon, SteamIcon, FoodIcon, CocaColaIcon, VegasIcon,NeptuneIcon, WheelIcon, UniqloIcon];
+  const imgArr = [
+    BbIcon,
+    LegoIcon,
+    SteamIcon,
+    FoodIcon,
+    CocaColaIcon,
+    NeptuneIcon,
+    WheelIcon,
+    GuitarIcon,
+    PopcornIcon,
+  ];
 
   useEffect(() => {
     const iconInterests = document.querySelector('.interests-icon');
@@ -18,22 +28,26 @@ const Interests = () => {
 
     // Show initial image
     iconInterests.src = imgArr[0];
-    
-    // Set image every 1 second by changing src of img 
+
+    // Set image every 1 second by changing src of img
     const interestsImgInterval = setInterval(() => {
-      // Set img src to img in position i of array 
+      // Set img src to img in position i of array
       iconInterests.src = imgArr[i];
-      // Update the counter 
+      // Update the counter
       i = (i + 1) % imgArr.length;
     }, 1000);
 
     return () => clearInterval(interestsImgInterval);
-  }, []); 
+  }, []);
 
   return (
-    <div className="interests-container">
+    <div className='interests-container'>
       <p>I like</p>
-      <img className='interests-icon' src={imgArr} alt="Icons of my interests" />
+      <img
+        className='interests-icon'
+        src={imgArr}
+        alt='Icons of my interests'
+      />
       <p>.</p>
     </div>
   );
