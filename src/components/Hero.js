7338b4499cb1242765a9770af73bbdd1c27fba38
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-08-18 12:21:50 +0100

    feat: add jest icon to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,46 +3,46 @@ import CssIcon from '../images/hero/css-icon.png';
 import JsIcon from '../images/hero/js-icon.png';
 import SassIcon from '../images/hero/sass-icon.png';
 import ReactIcon from '../images/hero/react-icon.png';
-import GitIcon from '../images/hero/git-icon.png';
+import JestIcon from '../images/hero/jest-icon.png';
 import TypescriptIcon from '../images/hero/typescript-icon.png';
 import ArrowDown from '../images/hero/down-arrow.svg';
 
-const Hero = ({scrollIntoViewOffset}) => {
+const Hero = ({ scrollIntoViewOffset }) => {
   return (
     <section id='hero'>
-      <div className="hero-container">
+      <div className='hero-container'>
         <h1 className='hero-name'>Kunal Patel</h1>
         <h1 className='hero-role'>Web Developer</h1>
         <div className='icon html-icon' title='HTML'>
-          <img className='html-img' src={HtmlIcon} alt="HTML icon" />
+          <img className='html-img' src={HtmlIcon} alt='HTML icon' />
         </div>
         <div className='icon css-icon' title='CSS'>
-          <img className='css-img' src={CssIcon} alt="CSS icon" />
+          <img className='css-img' src={CssIcon} alt='CSS icon' />
         </div>
         <div className='icon js-icon' title='JavaScript'>
-          <img className='js-img' src={JsIcon} alt="JavaScript icon" />     
+          <img className='js-img' src={JsIcon} alt='JavaScript icon' />
         </div>
         <div className='icon sass-icon' title='SASS'>
-          <img className='sass-img' src={SassIcon} alt="SASS icon" />
+          <img className='sass-img' src={SassIcon} alt='SASS icon' />
         </div>
         <div className='icon react-icon' title='React'>
-          <img className='react-img' src={ReactIcon} alt="React icon" />
+          <img className='react-img' src={ReactIcon} alt='React icon' />
         </div>
-        <div className='icon git-icon' title='Git'>
-          <img className='figma-img' src={GitIcon} alt="Figma icon" />
+        <div className='icon jest-icon' title='Jest'>
+          <img className='jest-img' src={JestIcon} alt='Jest icon' />
         </div>
         <div className='icon typescript-icon' title='Typescript'>
-          <img className='typescript-img' src={TypescriptIcon} alt="Typescript icon" />
+          <img className='typescript-img' src={TypescriptIcon} alt='Typescript icon'
+          />
         </div>
       </div>
       <div className='hero-recent-work-container'>
         <p>Take a look at my recent work</p>
         <button onClick={() => scrollIntoViewOffset('#projects', 100)}>
-          <img src={ArrowDown} alt="Scroll down to recent works" />
-        </button> 
+          <img src={ArrowDown} alt='Scroll down to recent works' />
+        </button>
       </div>
     </section>
-    
   );
 };
 
